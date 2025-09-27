547238bb281d88fa1aabab4a122d66f1f480b065
Tural <111038612+turalal@users.noreply.github.com>
2025-09-27 20:28:58 +0400

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://tg_db_owner:nVaf7lWv9HrB@ep-frosty-voice-a2s9itd4-pooler.eu-central-1.aws.neon.tech/tg_db?sslmode=require&channel_binding=require',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
